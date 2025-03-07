7f86d7a53a659d590aaf3b9e1347de7e51c3852e
monkeytickler812 <159182493+monkeytickler812@users.noreply.github.com>
2025-03-07 11:23:52 -0800

    health bar

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -2,7 +2,15 @@ class_name Player extends CharacterBody2D
 
 @export var speed := 400
 @export var drag_factor := 12.0
+@export var max_health := 10
+@onready var _health_bar: ProgressBar = %HealthBar
 
+var health := max_health: set = set_health
+
+func _ready() -> void:
+	_health_bar.max_value = max_health
+	_health_bar.value = health
+	_health_bar.init_health(health)
 
 func _physics_process(delta: float) -> void:
 	var move_direction := Input.get_vector("left", "right", "up", "down")
@@ -11,3 +19,11 @@ func _physics_process(delta: float) -> void:
 	velocity += steering * drag_factor * delta
 	var direction_discrete := move_direction.sign()
 	move_and_slide()
+
+func set_health(new_health: int) -> void:
+	var previous_health := health
+	health = clampi(new_health, 0, max_health)
+	_health_bar.value = health
+	if health == 0:
+		queue_free()
+	_health_bar.health = health
