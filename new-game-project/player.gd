f47a63e5bf3ddc0bddef00b40798df3757f28fac
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2025-03-07 12:01:03 -0800

    Installing Dialuge Manager (AssetLib)

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -11,3 +11,8 @@ func _physics_process(delta: float) -> void:
 	velocity += steering * drag_factor * delta
 	var direction_discrete := move_direction.sign()
 	move_and_slide()
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		DialogueManager.show_example_dialogue_balloon(load("res://main.dialogue"),"start")
+		return
