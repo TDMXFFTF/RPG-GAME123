e6f13920b78e730acd2e57de4163653fad09de95
GlutenFreeGrass <kawu11@s.sfusd.edu>
2025-03-24 12:03:18 -0700

    added script

---
+++ new-game-project/npc.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+signal quest_menu_closed
+var quest_test_active = false
+var quest_test_completed = false
+var object = 0
+
+func quest_chat():
+	pass
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
