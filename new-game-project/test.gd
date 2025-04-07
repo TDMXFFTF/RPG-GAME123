8e86296100c26d0654bdb4f78830fe51b72eceb0
GlutenFreeGrass <kawu11@s.sfusd.edu>
2025-04-07 14:43:30 -0700

    working test sidequest

--- new-game-project/test.gd
+++ new-game-project/test.gd
@@ -7,6 +7,10 @@ var quest1_done = false
 var quest1_active = false
 var test_quest = 0
 
+
+func test():
+	test_quest += 1
+	
 func on_yes_choice():
 	print("Yes clicked!") 
 	quest1_active = true
@@ -14,9 +18,10 @@ func on_yes_choice():
 	
 func on_no_choice():
 	print("No clicked!") 
-	test_quest += 1
 	
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("test"):
+		test()
 	if quest1_active:
 		if test_quest == 1:
 			print("you did it!")
