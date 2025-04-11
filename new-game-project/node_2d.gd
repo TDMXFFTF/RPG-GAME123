c2c9d7d208f656934463220890f0bb6d925c345d
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2025-04-11 11:59:01 -0700

    Random Spawning

--- new-game-project/node_2d.gd
+++ new-game-project/node_2d.gd
@@ -1,14 +1,43 @@
 extends Node2D
 
+@export var mob_scene: PackedScene = preload("res://mob.tscn")
+@export var spawn_area: Rect2 = Rect2(Vector2.ZERO, Vector2(400, 300))
+@export var spawn_interval: float = 1.0
+@export var stop_after: float = 5.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	randomize()
+
+	$SpawnTimer.wait_time = spawn_interval
+	$SpawnTimer.start()
+
+	$StopTimer.wait_time = stop_after
+	$StopTimer.one_shot = true
+	$StopTimer.start()
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_mob()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var aim_direction := Vector2.ZERO
-	aim_direction = global_position.direction_to(get_global_mouse_position())
-	if aim_direction.length() > 0.1:
-		rotation = aim_direction.angle()
+func _on_stop_timer_timeout() -> void:
+	$SpawnTimer.stop()
+	print("Stopped spawning mobs.")
+
+
+func spawn_mob():
+	if not mob_scene:
+		print("Mob scene not assigned!")
+		return
+
+	var mob = mob_scene.instantiate()
+
+	# NEED TO FIX RANDOM SPAWNING 
+	var spawn_x = randf_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x)
+	var spawn_y = randf_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
+	mob.position = Vector2(spawn_x, spawn_y)
+
+
+	print("Spawning mob at:", mob.position)
+
+
+	get_tree().current_scene.add_child(mob)
