bf642b9e0baab3978f3940082221ea994aab140a
monkeytickler812 <jazhu8@s.sfusd.edu>
2025-04-27 00:44:15 -0700

    health bar fix

--- new-game-project/weapon.gd
+++ new-game-project/weapon.gd
@@ -17,21 +17,12 @@ func _process(_delta: float) -> void:
 	if not is_visible_in_tree() or attack_active:
 		return
 
-	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
-	rotation = mouse_direction.angle()
-
-	if scale.y == 1 and mouse_direction.x < 0:
-		scale.y = -1
-	elif scale.y == -1 and mouse_direction.x > 0:
-		scale.y = 1
+	update_weapon_direction()
 
 	if Input.is_action_just_pressed("attack") and not attack_active:
 		start_attack()
 
-
-func start_attack():
-	attack_active = true
-
+func update_weapon_direction() -> void:
 	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
 	rotation = mouse_direction.angle()
 
@@ -39,6 +30,10 @@ func start_attack():
 		scale.y = -1
 	elif scale.y == -1 and mouse_direction.x > 0:
 		scale.y = 1
+
+func start_attack():
+	attack_active = true
+	update_weapon_direction()
 
 	animation_player.speed_scale = attack_speed
 	animation_player.play("sword")
@@ -47,21 +42,20 @@ func start_attack():
 	await get_tree().create_timer(1.0 / attack_speed).timeout
 	attack_active = false
 
+func check_hits():
+	if not attack_active:
+		return
 
-
+	for body in hit_box.get_overlapping_bodies():
+		if body is Mob and body not in hit_mobs:
+			hit_mobs.append(body)
+			if body.has_method("take_damage"):
+				body.take_damage(damage)
 
 func _on_attack_finished(anim_name: String) -> void:
 	attack_active = false
 	hit_mobs.clear()
-
-	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
-	rotation = mouse_direction.angle()
-
-	if scale.y == 1 and mouse_direction.x < 0:
-		scale.y = -1
-	elif scale.y == -1 and mouse_direction.x > 0:
-		scale.y = 1
-
+	update_weapon_direction()
 
 func _on_hit_box_body_entered(body: Node) -> void:
 	if not attack_active:
