bdd4e12cb1efd060dd079379a56987da822199a9
GlutenFreeGrass <kawu11@s.sfusd.edu>
2025-05-09 10:37:01 -0700

    gang

--- new-game-project/npc.gd
+++ new-game-project/npc.gd
@@ -2,9 +2,9 @@ extends StaticBody2D
 
 @onready var first_actionable := $Actionable
 @onready var second_actionable := $Actionable2
-
-
 @export var first_dialog : DialogueResource;
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 signal quest_menu_closed
 var quest_test_active = false
@@ -18,7 +18,7 @@ func quest_chat():
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	animated_sprite_2d.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
