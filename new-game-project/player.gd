1731278c2962248e8605c95b18c7d112a407b217
monkeytickler812 <159182493+monkeytickler812@users.noreply.github.com>
2025-05-15 18:40:28 -0700

    im gonna end it

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -128,5 +128,6 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		var actionables = actionable_finder.get_overlapping_areas()
 		if actionables.size() > 0:
-			actionables[0].action()
-			return
+			for actionable in actionables:
+				if (actionable is Actionable):
+					actionable.action()
